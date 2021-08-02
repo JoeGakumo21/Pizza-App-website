62fbd11de204442c75c2777c2685214a99c04ab8
Joe Gakumo <joegakumo1@gmail.com>
2021-08-02 04:09:14 +0300

    minor changes

--- main.js
+++ main.js
@@ -9,29 +9,39 @@ $(document).ready(function(){
         $(".menu-recipe").hide(10000);
     })
 
-    // getting values from user
-    $("#proceed-pizza").click(()=>{
-        // let customer=$("customerName").val();
-        let pizzaFlavour=$("#pizzaFlavor option:selected").val();
-        let crust=$("#pizzaCrust option:selected").val();
-        let pizzaToppings=$("#pizzaToppings option:selected").val();
-        let size=$("#izzaSize option:selected").val();
-        let number=$("#noOfPizza").val();
+    // // getting values from user
+    // $("#proceed-pizza").click(()=>{
+    //     // let customer=$("customerName").val();
+    //     let pizzaFlavour=$("#pizzaFlavor option:selected").val();
+    //     let crust=$("#pizzaCrust option:selected").val();
+    //     let pizzaToppings=$("#pizzaToppings option:selected").val();
+    //     let size=$("#izzaSize option:selected").val();
+    //     let number=$("#noOfPizza").val();
 
         // creating a constructor
-        function NewPizzaOrder(customer,pizzaFlavor, pizzaCrust, pizzaToppings, pizzaSize,noOfPizza,total){
-            this.nameOfCustomer=customer;
-            this.flavourOfPizza=pizzaFlavor;
-            this.pizzaCrust=pizzaCrust;
-            this.pizzaToppings=pizzaToppings;
-            this.pizzaSize=pizzaSize;
-            this.noOfPizza=noOfPizza;
-            this.total=total;
+        // function NewPizzaOrder(pizzaFlavor, pizzaCrust, pizzaToppings, pizzaSize,noOfPizza,total){
+        //     this.flavourOfPizza=pizzaFlavor;
+        //     this.pizzaCrust=pizzaCrust;
+        //     this.pizzaToppings=pizzaToppings;
+        //     this.pizzaSize=pizzaSize;
+        //     this.noOfPizza=noOfPizza;
+        //     this.total=total;
             
-        }
-        NewPizzaOrder();
-            });
-    })
+        // }
+        // NewPizzaOrder();
+
+        // NewPizzaOrder.prototype.detailes=function(){
+
+        // }
+
+
+        // 
+        $(".display").append("<li>" +"You have Ordered : " + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again."+"<li>");
+
+});
+
+
+
 function getpizzaFlavor(){
     var pizzaFlavor=document.getElementById("pizzaFlavor").value;
     return parseInt(pizzaFlavor);
@@ -55,9 +65,9 @@ function getQuantity() {
 function getTotalAmount() {
     var totalAmount = (getpizzaSize()+ getpizzaFlavor() + pizzaCrust() + pizzaToppings()) * getQuantity();
     alert("You have Ordered : " + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
-    prompt("KIndly indicate Your location")
-    alert("You delivery fee is Ksh. 250")
-    alert("You order will be delivered shortly")
-    alert("Continue shoppping with JoeKumsyPizzaIn each and every time .")
+    prompt("KIndly indicate Your location");
+    alert("You delivery fee is Ksh. 250 and the total charges will be "+ (totalAmount+250));
+    alert("You order will be delivered shortly");
+    alert("Continue shoppping with JoeKumsyPizzaIn each and every time .");
 }
 
